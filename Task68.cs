b7f92476d2e986e8dec5481c54c55f4f2881d49d
Platon265 <119070658+Platon265@users.noreply.github.com>
2023-01-03 19:43:43 +0300

    Create Task68.cs

---
+++ Task68.cs
@@ -0,0 +1,13 @@
+Console.WriteLine("Введите m");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите n");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(Ackermann(m, n));
+
+int Ackermann(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0) return Ackermann(m - 1, 1);
+    else return Ackermann(m - 1, Ackermann(m, n - 1));
+}
